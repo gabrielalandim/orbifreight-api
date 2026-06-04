8772510e0088f7cb1f71352466b3fddb5dac1b54
Administrador <Administrador@CAN-P89807925O>
2026-06-04 15:35:58 -0300

    chore: setup inicial do projeto spring boot no intellij

---
+++ src/main/java/br/com/fiap/orbifreight/OrbifreightApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.orbifreight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrbifreightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrbifreightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/orbifreight/OrbifreightApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.orbifreight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrbifreightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
